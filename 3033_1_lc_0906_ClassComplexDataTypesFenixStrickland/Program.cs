c4ce340957ce2f4f8f684c5ae1f7aa279d52d7e0
Joe Jennings <lane@lanestrickland.com>
2022-09-06 16:12:33 -0500

    Add project files.

---
+++ 3033_1_lc_0906_ClassComplexDataTypesFenixStrickland/Program.cs
@@ -0,0 +1,76 @@
+﻿//Fenix Strickland
+//Class Complex DataTypes
+
+
+int i;
+string str;
+
+str = new string("Fenix Strickland This is a str"); //102
+str = "another str"; //103
+
+i = 20;
+
+
+HotelRoom hm;
+hm = new HotelRoom();//110
+
+hm = new HotelRoom(2, false); //112
+HotelRoom hm2;
+hm2 = hm; //112
+
+HotelRoom hm1 = new HotelRoom(3, true);
+
+
+
+hm.nBeds = 2;
+
+// complex datatype
+// string 
+string str2 = new string("str1");
+str2 = "sTR2 this is mis 3033 001";
+
+// DateTime TimeSpan
+
+DateTime dt1;
+dt1 = new DateTime();
+
+dt1 = DateTime.Now;
+
+DateTime dt2 = new DateTime(2000,2,20,8,30,0);
+
+TimeSpan ts1 = dt1 - dt2;
+
+
+
+Console.ReadLine();
+
+//memberfields
+//
+
+public class HotelRoom
+{
+    // overload vs override
+    //member function = every room has 1
+    public HotelRoom()
+    {
+
+    }
+    // this
+    public HotelRoom(int nBeds, bool canSmoke) //overload
+    {
+        this.nBeds = nBeds;
+        this.canSmoke = canSmoke;
+
+    }
+
+    //datatype1 function_name()
+    //{
+    //    ...
+    //    return var;
+    //}
+    //member fields
+   public int nBeds;
+   public bool canSmoke;
+   public string coffeeMachine;
+   public static string restaurant;
+}
